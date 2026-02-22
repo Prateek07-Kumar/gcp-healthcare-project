3dfd5ef824dbb3ed047696c432ca9bb5ee3dfeeb
Prateek07-Kumar <prateekchaurasia791@gmail.com>
2026-02-22 23:05:21 +0530

    updated_cloudbuld

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "metal-vehicle-482506-n5"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-205bd6ad-bucket" ## change this 
+COMPOSER_BUCKET = "us-central1-demo-instance-1bfcd3f8-bucket" ## change this 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/hospitalA_mysqlToLanding.py"
 PYSPARK_JOB_1 = {
