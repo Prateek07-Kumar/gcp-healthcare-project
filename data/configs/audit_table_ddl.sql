ee6eb21254576515ae6ddbb2480dec3988f6c724
Prateek07-Kumar <prateekchaurasia791@gmail.com>
2026-02-22 19:37:25 +0530

    commit_updated_project

---
+++ data/configs/audit_table_ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE metal-vehicle-482506-n5.temp_dataset.audit_log (
+    data_source STRING,
+    tablename STRING,
+    load_type STRING,
+    record_count INT64,
+    load_timestamp TIMESTAMP,
+    status STRING
+);
